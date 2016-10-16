2b1fb184d1cad3aad2f6cbea1ace320eed543fe1
Shehab Abdel-Salam <shehab_m@aucegypt.edu>
2016-10-16 12:40:33 +0300

    Files Added.
    Hi. Added Skeleton. Bye.

---
+++ UDPClientSocket.hpp
@@ -0,0 +1,10 @@
+#ifndef UDPCLIENTSOCKET_H 
+#define UDPCLIENTSOCKET_H
+
+class UDPClientSocket : public UDPSocket {
+public:
+UDPClientSocket ();
+bool initializeClient (char * _peerAddr, int _peerPort); ~UDPClientSocket ( );
+};
+
+#endif // UDPCLIENTSOCKET_H
---
+++ UDPServerSocket.hpp
@@ -0,0 +1,11 @@
+
+#ifndef UDPSERVERSOCKET_H
+#define UDPSERVERSOCKET_H
+
+class UDPServerSocket : public UDPSocket {
+public:
+UDPServerSocket ();
+bool initializeServer (char * _myAddr, int _myPort); ~UDPServerSocket ( );
+};
+
+#endif // UDPSERVERSOCKET_H
