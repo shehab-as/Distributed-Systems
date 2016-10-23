83de22ffd14bfd46ee779d3d077e7183e3de502d
ShadyF <shadster09@gmail.com>
2016-10-23 15:34:00 +0200

    UDPSocket works but with an error occuring on recvfrom()

--- main.cpp
+++ main.cpp
@@ -5,23 +5,25 @@
 
 using namespace std;
 
-char message_received[2];
+char message_received[50];
 auto server = UDPSocket();
 auto client = UDPSocket();
 bool flag;
 
 void server_test() {
-    server.readFromSocketWithBlock(message_received, 8);
+    server.readFromSocketWithBlock(message_received, 50, 8);
 }
 
 void client_test() {
-    client.writeToSocket((char *) "hi", 8);
+        client.writeToSocket((char *) "FUCK YOU ZIZO 5od ya metnak", 8);
 }
 
 int main() {
     flag = server.initializeServer((char *) "localhost", 1234);
     flag = client.initializeClient((char *) "localhost", 1234);
 
+    char message2[50];
+    cout << sizeof(message2) << endl;
     std::thread t1(server_test);
     std::thread t2(client_test);
     t2.join();
