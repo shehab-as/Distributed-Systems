7526a21e2cdf6fddc5c20fa5dc4a726d92abcb6b
ShadyF <shadster09@gmail.com>
2016-10-29 16:43:58 +0200

    Some cleanup to Message.cpp

--- Message/Message.cpp
+++ Message/Message.cpp
@@ -1,26 +1,22 @@
-#include "MESSAGE_H"
-
+#include "Message.h"
 //Message Type: Request OR Reply
 
 //Attributes & Methods not needed in Exercise 1:
-	//operation
-	//rpc_id
+//operation
+//rpc_id
 
 //Constructor
 Message::Message(int op, void *p_message, size_t p_message_size, int p_rpc_id)
-{
-	this.operation = op;
-	this.message = &p_message;
-	this.message_size = p_message_size;
-	this.rpc_id = p_rpc_id;
+        : operation(op), message(p_message),
+          message_size(p_message_size),
+          rpc_id(p_rpc_id) {
+
 }
 
 //Marshalled Constructor
-Message::Message(char * marshalled_base64)
-{	}
+Message::Message(char *marshalled_base64) {}
 
-char* Message::marshal()
-{
+char *Message::marshal() {
 //	//To do.
 //	char *Marshy;
 //
@@ -28,34 +24,25 @@ char* Message::marshal()
 //	return Marshy;
 }
 
-int Message::getOperation()
-{	return operation;	}
+int Message::getOperation() { return operation; }
 
-int Message::getRPCId()
-{	return rpc_id;	}
+int Message::getRPCId() { return rpc_id; }
 
-void* Message::getMessage()
-{	return message;	}
+void *Message::getMessage() { return message; }
 
-size_t Message::getMessageSize()
-{	return message_size;	}
+size_t Message::getMessageSize() { return message_size; }
 
-MessageType Message::getMessageType()
-{	return message_type;	}
+MessageType Message::getMessageType() { return message_type; }
 
-void Message::setOperation(int op)
-{	this.operation = operation;	}
+void Message::setOperation(int op) { operation = op; }
 
-void Message::setMessage(void *msg, size_t msg_size)
-{
-	this->message = msg;
-	this.message_size = msg_size;
+void Message::setMessage(void *msg, size_t msg_size) {
+    message = msg;
+    message_size = msg_size;
 }
 
-void Message::setMessageType(MessageType msg_type)
-{	this.message_type = msg_type;	}
+void Message::setMessageType(MessageType msg_type) { message_type = msg_type; }
 
-Message::~Message()
-{
-	delete message;
+Message::~Message() {
+    delete message;
 }
