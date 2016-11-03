616c632cf89bad93786a6e7ca2e7d9ad7fc39228
zeyad <zyad224@aucegypt.edu>
2016-11-03 22:49:23 +0200

    Testing and running server and client in 2 different threads

--- main.cpp
+++ main.cpp
@@ -8,18 +8,37 @@
 
 using namespace std;
 
-// For server
-int main() {
-    auto server = Server((char *) "localhost", 1234, 10);
-}
 
 // For client
 int client() {
+    int x=5;
     auto client = Client();
     char *message = (char *) "~Server not ~server()";
     Message request(MessageType::Request, message, strlen(message), 0);
     auto reply = client.execute(&request, (char *) "localhost", 1234);
-    cout << reply.getMessage();
+    cout << (char*) reply.getMessage();
 
     return 0;
 }
+
+//for server
+int server()
+{
+     auto server = Server((char *) "localhost", 1234, 10);
+    return 0;
+
+}
+//Testing
+int main() {
+
+    std::thread c(client);
+    std::thread s(server);
+
+    c.join();
+    s.join();
+
+return 0;
+
+}
+
+
