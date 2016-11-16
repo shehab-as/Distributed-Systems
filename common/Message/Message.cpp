167463e93f11fcb120a8548113b085dfd54ae179
ShadyF <shadster09@gmail.com>
2016-11-16 14:34:23 +0200

    Imlpemented marshalling and unmarshalling in Message class

--- common/Message/Message.cpp
+++ common/Message/Message.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <sstream>
 #include "Message.h"
 //Message Type: Request OR Reply
 
@@ -8,15 +9,35 @@
 //rpc_id
 
 //Constructor
-Message::Message(int op, void *p_message, size_t p_message_size, int p_rpc_id)
-        : operation(op), message(p_message),
-          message_size(p_message_size),
+Message::Message(int op, std::vector<std::string> p_message, size_t p_message_size, int p_rpc_id)
+        : operation(op), parameters(p_message),
+          parameters_size(p_message_size),
           rpc_id(p_rpc_id) {
 
 }
 
 //Marshalled Constructor
-Message::Message(char *marshalled_base64) {}
+Message::Message(char *marshalled_base64) {
+    std::stringstream tokenizer(marshalled_base64);
+    std::string token;
+
+    tokenizer >> token;
+    setOperation(std::stoi(token));
+
+    tokenizer >> token;
+    setMessageType((MessageType) std::stoi(token));
+
+    tokenizer >> token;
+    setRPCId(std::stoi(token));
+
+    tokenizer >> token;
+    setParamsSize((size_t) std::stoi(token));
+
+    for (int i = 0; i < getParamsSize(); i++) {
+        tokenizer >> token;
+        parameters.push_back(token);
+    }
+}
 
 // Marshalled Message should be of the following format:
 // "operation MessageType rpc_id num_of_params param1 param2 ... message_size"
@@ -25,6 +46,11 @@ std::string Message::marshal() {
     marshalled_msg.append(std::to_string(getOperation()) + " ");
     marshalled_msg.append(std::to_string(getMessageType()) + " ");
     marshalled_msg.append(std::to_string(getRPCId()) + " ");
+    marshalled_msg.append(std::to_string(getParamsSize()) + " ");
+
+    for (int i = 0; i < getParamsSize(); i++)
+        marshalled_msg.append(parameters[i] + " ");
+
     return marshalled_msg;
 }
 
@@ -32,21 +58,29 @@ int Message::getOperation() { return operation; }
 
 int Message::getRPCId() { return rpc_id; }
 
-void *Message::getMessage() { return message; }
+std::vector<std::string> Message::getParams() { return parameters; }
 
-size_t Message::getMessageSize() { return message_size; }
+size_t Message::getParamsSize() { return parameters_size; }
 
 MessageType Message::getMessageType() { return message_type; }
 
 void Message::setOperation(int op) { operation = op; }
 
-void Message::setMessage(void *msg, size_t msg_size) {
-    message = msg;
-    message_size = msg_size;
+void Message::setMessage(std::vector<std::string> params, size_t params_size) {
+    parameters = params;
+    parameters_size = params_size;
 }
 
 void Message::setMessageType(MessageType msg_type) { message_type = msg_type; }
 
 Message::~Message() {
 
+}
+
+void Message::setRPCId(int _rpc_id) {
+    rpc_id = _rpc_id;
+}
+
+void Message::setParamsSize(size_t _params_size) {
+    parameters_size = _params_size;
 }
--- common/Message/Message.h
+++ common/Message/Message.h
@@ -2,6 +2,7 @@
 #define MESSAGE_H
 
 #include <cstddef>
+#include <vector>
 
 enum MessageType {
     Request, Reply
@@ -12,12 +13,12 @@ class Message {
 private:
     MessageType message_type;
     int operation;      // RPC Operation
-    void *message;      // parameters, vector<void *> one for each param
-    size_t message_size;
+    std::vector<std::string>parameters;      // parameters, vector<void *> one for each param
+    size_t parameters_size;
     int rpc_id;
 
 public:
-    Message(int operation, void *p_message, size_t p_message_size, int p_rpc_id);
+    Message(int operation, std::vector<std::string>_params, size_t _params_size, int p_rpc_id);
 
     // Unmarshalling occurs here
     explicit Message(char *marshalled_base64);
@@ -28,18 +29,21 @@ public:
 
     int getRPCId();
 
-    void *getMessage();
+    std::vector<std::string>getParams();
 
-    size_t getMessageSize();
+    size_t getParamsSize();
 
     MessageType getMessageType();
 
     void setOperation(int _operation);
 
-    void setMessage(void *message, size_t message_size);
+    void setMessage(std::vector<std::string>params, size_t params_size);
 
     void setMessageType(MessageType message_type);
 
+    void setParamsSize(size_t _params_size);
+
+    void setRPCId(int _rpc_id);
     ~Message();
 
 };
