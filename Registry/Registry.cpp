5c582ae272377498ae39aa88d94ead53a261f7bc
farida <faridasoliman@aucegypt.edu>
2016-12-11 13:23:19 +0000

    fixed case enum

--- Registry/Registry.cpp
+++ Registry/Registry.cpp
@@ -204,7 +204,7 @@ void Registry::handleRequest(Message request, sockaddr_in sender_addr) {
 
         }
 
-        case UPDATE_User_VIEWS: {
+        case UPDATE_USER_VIEWS: {
             std::vector<std::string> params, reply_params;
 
             params = request.getParams();
